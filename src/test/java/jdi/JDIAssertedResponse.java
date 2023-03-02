7effafff92b65c66954edd02fe926900a8588ca7
Windostre <windostre@gmail.com>
2023-03-02 15:53:35 +0300

    JDIAssertedResponse added assertions

--- src/test/java/jdi/JDIAssertedResponse.java
+++ src/test/java/jdi/JDIAssertedResponse.java
@@ -1,6 +1,10 @@
 package jdi;
 
 import com.epam.http.response.RestResponse;
+import model.user.Message;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
 
 public class JDIAssertedResponse {
     public final RestResponse response;
@@ -8,4 +12,24 @@ public class JDIAssertedResponse {
     public JDIAssertedResponse(RestResponse response) {
         this.response = response;
     }
+
+    //для десериализации json в pojo
+    public <T> T as(String jsonPath, Class<T> tClass) {
+        return response.getRaResponse().then().extract().body().jsonPath().getObject(jsonPath, tClass);
+    }
+
+    //получаем сообщение и статус из json блока info
+    public JDIAssertedResponse hasMessage(String expectedMessage) {
+        Message message = as("info", Message.class);
+        assertEquals(message.getMessage(), expectedMessage);
+        return this;
+    }
+
+    public JDIAssertedResponse hasStatusCode(int expectedStatus) {
+        int actualStatus = response.getStatus().code;
+        assertEquals(actualStatus, expectedStatus);
+        return this;
+
+    }
+
 }
