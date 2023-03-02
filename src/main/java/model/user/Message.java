3d9eb2b788bc7d9434e49482af88a6530a3717b1
Windostre <windostre@gmail.com>
2023-03-02 15:42:28 +0300

    Message class for data description added

---
+++ src/main/java/model/user/Message.java
@@ -0,0 +1,13 @@
+package model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String status;
+    private String message;
+}
