f28bc6333debef75c44d22e806de929c9437b1de
Windostre <windostre@gmail.com>
2023-03-02 15:22:42 +0300

    UserController added base methods and endpoints

--- src/test/java/jdi/UserController.java
+++ src/test/java/jdi/UserController.java
@@ -1,7 +1,25 @@
 package jdi;
 
-import com.epam.http.annotations.ServiceDomain;
+import com.epam.http.annotations.*;
+import com.epam.http.requests.RestMethod;
+
+import static io.restassured.http.ContentType.JSON;
 
 @ServiceDomain("85.192.34.140:8080/api")
 public class UserController {
+    @POST("/register")
+    @ContentType(JSON)
+    private RestMethod registerUser;
+
+    @GET("/user")
+    @ContentType(JSON)
+    private RestMethod getUserInfo;
+
+    @PUT
+    @ContentType(JSON)
+    private RestMethod updateUserPassword;
+
+    @DELETE
+    @ContentType(JSON)
+    private RestMethod deleteUser;
 }
